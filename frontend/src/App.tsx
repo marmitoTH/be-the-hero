8e03d600b759b5a3baedf595b79105c085cdb608
Lucas <marmitoth@outlook.com>
2020-12-01 16:02:14 -0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import GlobalStyles from './assets/styles/global'
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      Hello World!
+      <GlobalStyles />
+    </div>
+  )
+}
+
+export default App;
